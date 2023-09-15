7087ac7799f28910f2445904b292fd9c72ece6b2
AsifRaza72 <asifaligohari572@gmail.com>
2023-09-16 01:25:59 +0500

    new

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,4 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+// import ExpenseItem from "./components/ExpenseItem";
+//  import ExpenseDate from "./components/ExpensesDate";
+ import Expenses from "./components/Expenses"
 function App() {
   const expenses=[
     {title:"Car Insurance", date:new Date(2023,9,1), amount:6000},
@@ -8,9 +10,7 @@ function App() {
   ]
   return (
     <div>
-    <ExpenseItem title={expenses[0].title} date={expenses[0].date} amount={expenses[0].amount}></ExpenseItem>
-    <ExpenseItem title={expenses[1].title} date={expenses[1].date} amount={expenses[1].amount} ></ExpenseItem>
-    <ExpenseItem title={expenses[2].title} date={expenses[2].date} amount={expenses[2].amount} ></ExpenseItem>
+    <Expenses items={expenses} />
     </div>
     
   
--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
 import "./ExpenseItem.css"
+import ExpenseDate from "./ExpensesDate";
+
 export function ExpenseItem(props){
     return(
         <div className="expense-item">
-
-        <div >{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
             <h2>{props.title}</h2>
-
+        <div className="expense-item_price">Rs. {props.amount}/-</div>
         </div>
-        <div className="expense-item_price">{props.amount}</div>
         </div>
     );
 }
